bd428c31ca4848a45a3b76d3b988b5ee1c41d6a2
devBK84 <bk@kharashfeh.com>
2022-11-13 14:09:41 +0100

    Update IntegrationsTests KanbanController

--- src/test/java/com/example/backend/controller/KanbanBoardControllerIntergrationTest.java
+++ src/test/java/com/example/backend/controller/KanbanBoardControllerIntergrationTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +27,7 @@ public class KanbanBoardControllerIntergrationTest {
 
 
     @Test
+    @DirtiesContext
     void getAllTodos_thanReturnList() throws Exception {
         Todo todo = new Todo("8", "Chilipaste", "open");
         todoRepository.addTodo(todo);
@@ -39,6 +43,35 @@ public class KanbanBoardControllerIntergrationTest {
                                 "status": "open"
                                 }
                                 ]
+                                """
+                ));
+
+    }
+
+    @Test
+    @DirtiesContext
+    void editToDo_getByID_ThenEditToDoAndReturn() throws Exception {
+        Todo todo1 = new Todo("85", "Chilipaste", "open");
+        todoRepository.addTodo(todo1);
+
+        mockMvc.perform(put("http://localhost:8080/api/todo/" + todo1.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                        {
+                                        "id": "85",
+                                        "description": "Gin",
+                                        "status": "doing"
+                                        }
+                                        """
+                        )).andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                 {
+                                        "id": "85",
+                                        "description": "Gin",
+                                        "status": "doing"
+                                                                }
                                 """
                 ));
 
